8eb3f0fd57df38223568db2fc0086307606fd4a1
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-01-09 23:31:28 -0300

    FEAT(promise_3): Criando outro teste de promisse

---
+++ 00_Anotacoes/funcoes/promise_3.js
@@ -0,0 +1,18 @@
+function gerarNumerosEntre(min,max){
+    if(min > max){
+        // Desestruturação de array
+        [max,min] = [min,max]
+    }
+
+    return new Promise(resolve => {
+        const fator = max - min + 1
+        const aleatorio = parseInt(Math.random() * fator) + min
+        resolve(aleatorio)
+    })
+}
+
+gerarNumerosEntre(1,60)
+    .then(num => num * 10)
+    .then(numX10 => `O número gerado foi ${numX10}`)
+    .then(console.log)
+
