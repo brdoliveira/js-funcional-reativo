5c00b48e6131a28031f34ae49eb10cbc5391e199
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-09-15 00:10:16 -0300

    FEAT(funcoes): Criando função removerSeIncluir()

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -28,17 +28,22 @@ function lerArquivos(caminhos){
     return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
 }
 
-function elementosTerminadosCom(array, padrao){
-    return array.filter(el => el.endsWith(padrao))
+function elementosTerminadosCom(array, padraoTextual){
+    return array.filter(el => el.endsWith(padraoTextual))
 }
 
 function removerSeVazio(array){
   return array.filter(el => el.trim())
 }
 
+function removerSeIncluir(array, padraoTextual){
+  return array.filter(el => !el.includes(padraoTextual))
+}
+
 module.exports = {
   lerDiretorio,
   lerArquivos,
   elementosTerminadosCom,
-  removerSeVazio
+  removerSeVazio,
+  removerSeIncluir
 };
