95748b73eb7bb95827679424d8d7ab95f50ebb66
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-09-28 01:07:12 -0300

    REFACT(funcoes): Refatorando map()

--- funcional/imutabilidade_1.js
+++ funcional/imutabilidade_1.js
@@ -5,3 +5,6 @@ function ordenar(array){
 const nums = Object.freeze([3,1,7,9,4,6])
 const numsOrdenados = ordenar(nums)
 console.log(nums, numsOrdenados)
+
+const parteDosNumeros = nums.slice(2)
+console.log(parteDosNumeros,nums)
