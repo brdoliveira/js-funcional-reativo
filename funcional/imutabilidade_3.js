9fdc0c552bcfd2b6c3d100ca17c06568e3ab9cac
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-10-03 23:50:10 -0300

    FEAT(imutabilidade_3): Adicionando exemplo de atrubuição por valor

--- funcional/imutabilidade_3.js
+++ funcional/imutabilidade_3.js
@@ -1,15 +1,27 @@
 const pessoa = {
     nome: 'Maria',
     altura: 1.76,
-    cidade: 'São Paulo'
+    cidade: 'São Paulo',
+    end: {
+        rua: 'Feliz!'
+    }
 }
 
-// Atribuição por Referência
-function alteraPessoa(novaPessoa){
+// Atribuição por Referência (Função Impura!)
+function alteraPessoa(pessoa){
+    const novaPessoa = {...pessoa}
     novaPessoa.nome = 'João'
     novaPessoa.cidade = 'Fortaleza'
-
+    novaPessoa.end.rua = 'ABC'
     return novaPessoa
 }
 
 console.log(alteraPessoa(pessoa))
+
+let a = 3
+let b = a // Atribuição por valor (copia)
+
+a++
+b--
+
+console.log(a,b)
