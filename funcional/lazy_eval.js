85093feb36a92d50a48d501ef732c083d006fb15
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-10-06 20:32:51 -0300

    FEAT(lazy_eval): Adicionando exemplo de Lazy Evaluation

---
+++ funcional/lazy_eval.js
@@ -0,0 +1,34 @@
+function eager(a ,b){
+    // processamento mais pesado
+    const fim = Date.now() + 2500
+    while(Date.now() < fim){}
+
+    const valor = Math.pow(a,3)
+    return valor + b
+}
+
+console.time('#1')
+console.log(eager(3,100))
+console.log(eager(3,200))
+console.log(eager(3,300))
+console.timeEnd('#1')
+
+function lazy(a){
+    // processamento mais pesado
+    const fim = Date.now() + 2500
+    while(Date.now() < fim){}
+
+    const valor = Math.pow(a,3)
+    return function(b){
+        return valor + b
+    }
+}
+
+console.time('#1')
+
+var lazy3 = lazy(3)
+
+console.log(lazy3(100))
+console.log(lazy3(200))
+console.log(lazy3(300))
+console.timeEnd('#1')
