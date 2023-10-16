da372fc54f0094d75de216049e62c46efad94a3a
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-10-15 23:13:15 -0300

    FEAT(stream_dados_3): Importando rjxs e implementando exemplo

---
+++ rxjs/stream_dados_3.js
@@ -0,0 +1,11 @@
+const { interval } = require('rxjs')
+
+const gerarNumeros = interval(500)
+
+const sub1 = gerarNumeros.subscribe(num => {
+    console.log(Math.pow(2,num))
+})
+const sub2 = gerarNumeros.subscribe()
+
+setTimeout(() => sub1.unsubscribe(), 8000)
+setTimeout(() => sub2.unsubscribe(), 6000)
